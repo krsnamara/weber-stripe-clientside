bfef4a5fadf113b8f438d28ad23f3d4c8c683f60
krsnamara <martinj.fitzpatrick@gmail.com>
2023-04-20 09:59:57 -0400

    pay updated

--- src/Pay.jsx
+++ src/Pay.jsx
@@ -10,20 +10,25 @@ const Pay = () => {
                 justifyContent: "center",
             }}
         >
-            <button
-                style={{
-                    border: "none",
-                    width: 120,
-                    borderRadius: 5,
-                    paddidng: "20px",
-                    backgroundColor: "black",
-                    color: "white",
-                    fontWeight: "600",
-                    cursor: "pointer",
-                }}
+            <StripeCheckout 
+                name="Weber Shop" 
+                image="https://dw-images.weber.com/base/weber-logo.svg?auto=compress,format"
             >
-                Pay Now
-            </button>
+                <button
+                    style={{
+                        border: "none",
+                        width: 120,
+                        borderRadius: 5,
+                        paddidng: "20px",
+                        backgroundColor: "black",
+                        color: "white",
+                        fontWeight: "600",
+                        cursor: "pointer",
+                    }}
+                    >
+                    Pay Now
+                </button>
+            </StripeCheckout>
         </div>
     )
 }
